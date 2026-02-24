dc9f3bbb5bb99aea6751c2338109c2717be3ae1e
Joao-Pedro <joaopmalvarez@hotmail.com>
2026-02-24 13:43:39 +0000

    aula 2

---
+++ Aula02/Contador.java
@@ -0,0 +1,20 @@
+public class Contador {
+    int c;
+
+    public Conta() {
+        c = 0;
+    } 
+
+    public void inc() {
+        c++;
+    }
+
+    public void dec() {
+        c--;
+    }
+
+    public void res() {
+        c = 0;
+    }
+
+    
