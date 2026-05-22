3a90cf237e4b2464a85b1b9346ca90948bf29392
Joao-Pedro <joaopmalvarez@hotmail.com>
2026-05-22 13:10:17 +0000

    c

--- Aula_do_Ivan/Exercícios_Para_N2/A/A_LinkedList.java
+++ Aula_do_Ivan/Exercícios_Para_N2/A/A_LinkedList.java
@@ -75,9 +75,9 @@ public class A_LinkedList <T> {
 
     public boolean insert(T data, int pos) {
         NodeA<T> aux = new NodeA<T>(data, null);
+        NodeA<T> antes = get(pos - 1);
         int cont = 1;
         NodeA<T> pAnda = new NodeA<T>();
-        NodeA<T> pAndinha = new NodeA<T>(null, pAnda);
         if(!isFull()) {
             if (isEmpty()) {
                 head = tail = aux;
@@ -87,10 +87,74 @@ public class A_LinkedList <T> {
                     pAnda = pAnda.getNext();
                     cont++;
                 }
-                pAnda = aux;
+                aux.setNext(pAnda.getNext());
+                // atualizar o anterior para apontar para o setNext
+                antes.setNext(aux);
             }
             size++;
             return true;
         } else return false;
     }
+
+    public boolean removeFirst() {
+        if(!isEmpty()) {
+            NodeA<T> aux = head.getNext();
+            head.setNext(null);
+            head = aux;
+            size--;
+            return true;
+        } else return false;
+    }
+
+    public boolean removeLast() {
+        if (!isEmpty()) {
+            NodeA<T> aux = get(size - 1);
+            tail = aux;
+            aux.setNext(null);
+            size--;
+            return true;
+        } else return false;
+    } 
+
+    public boolean remove(int pos) {
+        if (!isEmpty()) {
+            NodeA<T> aux = get(pos);
+            NodeA<T> antes = get(pos - 1);
+            antes.setNext(aux.getNext());
+            aux.setNext(null);
+            size--;
+            return true;
+        } else return false;
+    }
+
+    public void print() {
+        System.out.println("Lista: [");
+        if (!isEmpty()) {
+            NodeA<T> pAnda;
+            pAnda = head;
+            while(pAnda.getNext() != null) {
+                System.out.println(pAnda.getData());
+                pAnda = pAnda.getNext();
+            }
+        }
+        System.out.println("], tamanho =" + size);
+    }
+
+    public void clear() {
+        if (!isEmpty()) {
+            NodeA<T> pAnda;
+            pAnda = head;
+            NodeA<T> aux = null;
+            while(size != 0) {
+                aux = pAnda;
+                pAnda = pAnda.getNext();
+                aux.setNext(null);
+                size--;
+            }
+        }
+    }
+
+//    public T pullFirst()
+//    public T pullLast()
+//    public T pull()
 }
